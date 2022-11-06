86adcc9c7e4a9eb8203f585d9b19152f8364a1d7
Sabrina Harvey <harveysabrina81@gmail.com>
2022-11-06 14:04:28 -0500

    Completed Exercise 2

---
+++ app.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+    let search = document.getElementsByClassName("btn")[0];
+
+    search.addEventListener("click", function(element){
+        element.preventDefault();
+
+        var httpReq = new XMLHttpRequest();
+
+        var url = "superheroes.php";
+
+        httpReq.onreadystatechange = function(){
+            if(httpReq.readyState === XMLHttpRequest.DONE){
+                if(httpReq.status === 200){
+                    alert(this.responseText);
+                    document.querySelector("#container").innerHTML = this.responseText;
+                    
+                }
+            
+                else{
+                    alert('There was a problem with the request.');
+                }
+            }
+        }
+        httpReq.open('GET', url, true);
+        httpReq.send();
+        
+    });
+}
+
